7ec9eab2044c13b6ef81e6348b903159c82e915d
Aleh <OBeketa@synergy.ru>
2025-06-16 20:05:24 +0200

    Collect particles to container under Def

--- Assets/Project/Client/Src/com/AB/GamePlay/Common/Particles/ParticleInstaller.cs
+++ Assets/Project/Client/Src/com/AB/GamePlay/Common/Particles/ParticleInstaller.cs
@@ -9,12 +9,15 @@ namespace Project.Client.Src.com.AB.GamePlay.Common.Particles
     public class ParticleInstaller : MonoInstaller
     {
         [InlineEditor] [SerializeField] private ScriptableObject[] _mappers;
-        IParticleMapper[] Mappers => _mappers.Select(item => (IParticleMapper) item).ToArray();
+        IParticleMapper[] Mappers => _mappers.Select(item => (IParticleMapper)item).ToArray();
 
         const string PARTICLE_TRANSFORM_GROUP = "PartilcePool";
 
         public override void InstallBindings()
         {
+            Container.BindInstance(transform).WithId(ParticlesConst.PARTICLES_CONTAINER_KEY);
+            var goPoolContainer = CreatePoolContainer();
+
             foreach (var mapper in Mappers)
             foreach (var item in mapper.GetParticleMapping())
             {
@@ -22,7 +25,7 @@ namespace Project.Client.Src.com.AB.GamePlay.Common.Particles
                     .WithId(item.Key)
                     .WithMaxSize(item.Def.MaxInstances)
                     .FromComponentInNewPrefab(item.Def.ParticlePrefab)
-                    .UnderTransformGroup(PARTICLE_TRANSFORM_GROUP);
+                    .UnderTransform(goPoolContainer.transform);
             }
 
             Container.Bind<ParticleService>().FromMethod(ctx =>
@@ -31,10 +34,17 @@ namespace Project.Client.Src.com.AB.GamePlay.Common.Particles
 
                 foreach (var mapper in Mappers)
                 foreach (var item in mapper.GetParticleMapping())
-                        pools[item.Key] = (Container.ResolveId<ParticleMono.Pool>(item.Key), item.Def);
+                    pools[item.Key] = (Container.ResolveId<ParticleMono.Pool>(item.Key), item.Def);
 
                 return new ParticleService(pools);
             }).AsSingle().NonLazy();
+        }
+
+        GameObject CreatePoolContainer()
+        {
+            GameObject goPoolContainer = new GameObject(PARTICLE_TRANSFORM_GROUP);
+            goPoolContainer.transform.parent = this.transform;
+            return goPoolContainer;
         }
     }
 }
---
+++ Assets/Project/Client/Src/com/AB/GamePlay/Common/Particles/ParticlesConst.cs
@@ -0,0 +1,7 @@
+namespace Project.Client.Src.com.AB.GamePlay.Common.Particles
+{
+    public static class ParticlesConst
+    {
+        public const string PARTICLES_CONTAINER_KEY = "ParticleContainer";
+    }
+}
